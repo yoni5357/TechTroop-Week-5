228ad1a98153775488f8a4b6cd86dd55b0f48d04
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-26 14:23:52 +0300

    express ex2 done

--- Express/ex1/server.ts
+++ Express/ex1/server.ts
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 3000;
 
+const store = [
+    { name: "table", inventory: 3, price: 800 },
+    { name: "chair", inventory: 16, price: 120 },
+    { name: "couch", inventory: 1, price: 1200 },
+    { name: "picture frame", inventory: 31, price: 70 }
+]
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
@@ -12,3 +19,10 @@ app.get('/', (req,res) => {
     res.send("Server is up and running smoothly");
 })
 
+app.get('/priceCheck/:itemName', (req,res) => {
+    const item = store.find(item => item.name === req.params.itemName)
+    if(!item){
+        res.send("item does'nt exist")
+    }
+    else res.send({"price":item.price});
+})
