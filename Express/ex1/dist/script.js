8d989fd285b20fdaab8cace8035a7463517f0070
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-26 16:43:04 +0300

    express extension 1 done

--- Express/ex1/dist/script.js
+++ Express/ex1/dist/script.js
@@ -1,4 +1,6 @@
 
+let money = 1000;
+
 async function checkPrice(){
     const itemName = document.getElementById('item-input').value;
     console.log(itemName);
@@ -7,18 +9,38 @@ async function checkPrice(){
     document.getElementById('display').innerHTML = price.price;
 }
 
+async function checkPriceBeforeBuy(){
+    const itemName = document.getElementById('buy-item-input').value;
+    console.log(itemName);
+    let response = await fetch(`http://localhost:3000/priceCheck/${itemName}`);
+    let price = await response.json();
+    if(money - price.price > 0){
+        return price.price;
+    }
+    else{
+        document.getElementById('buy-display').innerHTML = "no money get a job";
+    }
+}
+
 async function buyItem(){
     const itemName = document.getElementById('buy-item-input').value;
+    const price = await checkPriceBeforeBuy()
+    if(!price){
+        return;
+    }
     let response = await fetch(`http://localhost:3000/buy/${itemName}`);
     if(!response.ok){
         document.getElementById('buy-display').innerHTML = "Item does not exist";
         return;
     }
+    money -= price;
+    document.getElementById('money').innerHTML = money;
     let item = await response.json();
     document.getElementById('buy-display').innerHTML = `Congratulations, you've just bought ${item.name} for ${item.price}. There are ${item.inventory} left now in the store.`
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('money').innerHTML = money;
     document.getElementById('price-button').addEventListener('click',checkPrice);
     document.getElementById('buy-button').addEventListener('click', buyItem);
 })
