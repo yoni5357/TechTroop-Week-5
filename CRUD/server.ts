aaf86be1c7e486865fb62e1c3352f958dc6efa34
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-27 17:44:33 +0300

    crud ex2 done

--- CRUD/server.ts
+++ CRUD/server.ts
@@ -8,6 +8,15 @@ server.get("/sanity", (req, res) => {
     res.send("Server is up and running");
 })
 
+//Ex2
+const wordCounter = {"blue": 5};
+
+server.get("/:word", (req,res) => {
+    const word = req.params.word
+    if(wordCounter[word]){
+        res.send({count:wordCounter[word]})
+    } else res.send({count:0});
+})
 
 server.listen(port, () => {
     console.log("Server running on port " + port);
